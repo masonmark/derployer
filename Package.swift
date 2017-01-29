b69206cba9a534e05cbdc288aedba92f162889a8
Mason Mark <mason@fivespeed.com>
2017-01-29 21:03:09 +0900

    Add dependency on Mason.swift

--- Package.swift
+++ Package.swift
@@ -3,3 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Derployer"
 )
+
+package.dependencies = [
+    .Package(url: "https://github.com/masonmark/Mason.swift", majorVersion: 8),
+]
